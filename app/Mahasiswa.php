d40db51cd40907341cff66681f99a5d91bf3df25
randomize721 <anggadp91@gmail.com>
2018-11-06 23:03:31 +0800

    Relationship mahasiswa prodi

--- app/Mahasiswa.php
+++ app/Mahasiswa.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa extends Model
 {
     //
+    public function prodi()
+    {
+        return $this->belongsTo('App\Prodi');
+    }
 }
--- app/Prodi.php
+++ app/Prodi.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class prodi extends Model
 {
     //
+    public function mahasiswa()
+    {
+        return $this->hasMany('App\Mahasiswa');
+    }
 }
